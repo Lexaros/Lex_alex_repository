ac4728b4951f92bfdc8051109bd524c88ee735e6
Lexaros <AlexeiJanalov@gmail.com>
2015-04-29 21:56:00 +0300

    update calc_sum.php and print_name.php

--- calc_sum.php
+++ calc_sum.php
@@ -1,5 +1,3 @@
 <?php
-    $arg1=isset ( $_GET['arg1'] );
-    $arg2=isset ( $_GET['arg2'] );
-    $sum=$_GET["arg1"]+$_GET["arg2"];
-    echo $sum;
+    $sum = $_GET["arg1"] + $_GET["arg2"];
+    echo 'Summa = '.($sum);
