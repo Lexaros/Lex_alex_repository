2a195036ea1b5319cb83035eb6f95716b5b723a7
Lexaros <alexeijanalov@gmail.con>
2015-05-20 21:55:24 +0400

    update 1.2

---
+++ print_query_string.php
@@ -0,0 +1,3 @@
+<?php
+    header ("Content-Type: texp/plain");
+    echo "Query string= '" . $_SERVER["QUERY_STRING"] . "'";
---
+++ sum.php
@@ -0,0 +1,5 @@
+<?php
+    require_once('include/common.inc.php');
+    $arg1= GetParamFromGet( 'arg1', 0);
+    $arg2= GetParamFromGet( 'arg2', 0);
+    echo Sum($arg1, $arg2);
